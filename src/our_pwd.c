cd1a920e60fedc04b38ae9c4d699310d12788ba7
goulem <goulem@Goulem.com>
2023-08-08 22:34:24 +0100

    work on bult-in (env)

---
+++ src/our_pwd.c
@@ -0,0 +1,44 @@
+#include "minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int our_pwd(t_cmd *command)
+{
+    // Declare a character pointer to hold the directory path
+    char *directory_path;
+
+    // Ignore the command parameter (it's unused in this implementation)
+    (void)command;
+
+    // Allocate memory to store the directory path
+    directory_path = (char *)malloc(PATH_MAX);
+
+    // Check if memory allocation was successful
+    if (!directory_path)
+    {
+        perror("minishell: pwd: malloc");
+        check.exit_status = EXIT_FAILURE;
+        return (-1);
+    }
+
+    // Get the current working directory and store it in 'directory_path'
+    if (getcwd(directory_path, PATH_MAX) == NULL)
+    {
+        perror("minishell: pwd: ");
+        free(directory_path);
+        check.exit_status = EXIT_FAILURE;
+        return (-1);
+    }
+
+    // Print the current working directory to stdout
+    printf("%s\n", directory_path);
+
+    // Free the dynamically allocated memory for 'directory_path'
+    free(directory_path);
+
+    // Set the exit status to indicate success
+    check.exit_status = EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
+}
+
