7d124af7ee4edfca05fff8f6c143a980791a8d48
Ayoub Et-tass <aet-tass@c1r8p6.1337.ma>
2023-08-22 21:16:24 +0100

    fix cd bult-in

--- src/our_cd.c
+++ src/our_cd.c
@@ -16,93 +16,35 @@ void display_chdir_error(const char *path)
     perror("");
 }
 
-// Custom implementation of the cd command
-int our_cd(t_cmd *command, char ***environment)
-{
-    // Declare variables
-    t_environment *env_vars; // Stores environment variables as a linked list
-    char *current_working_dir = NULL; // Stores the current working directory path
-    char *old_working_dir = NULL; // Stores the previous working directory path
-
-    // Initialize environment variables by creating a linked list
-    env_vars = create_env_vars(*environment);
-
-    // Allocate memory for dynamic strings to store paths
-    current_working_dir = (char *)malloc(PATH_MAX); // Allocate space for current working directory path
-    if (!current_working_dir)
-    {
-        perror("Failed to allocate memory for current_working_dir");
-        return -1; // Return an appropriate error code
+// Function to change the current working directory
+int change_directory(const char *path, char ***environment) {
+    if (chdir(path) != 0) {
+        display_chdir_error(path);
+        check.exit_status = 1;
+        return 1;
     }
+    return 0;
+}
 
-    old_working_dir = (char *)malloc(PATH_MAX); // Allocate space for previous working directory path
-    if (!old_working_dir)
-    {
-        perror("Failed to allocate memory for old_working_dir");
-        free(current_working_dir); // Clean up previously allocated memory
-        return -1; // Return an appropriate error code
-    }
-
-    // Handle cd command with or without arguments
-    if (!command->args[1])
-    {
-        // If no arguments are provided, change to the HOME directory
-        if (chdir(expand_vars("$HOME", *environment)) != 0)
-        {
-            // Display error message using the custom function
-            display_chdir_error(expand_vars("$HOME", *environment));
-
-            // Set the exit status to indicate an error
-            check.exit_status = 1;
-            return 1; // Return an error code
-        }
-    }
-    else
-    {
-        // Change to the specified directory if an argument is provided
-        if (chdir(command->args[1]) != 0)
-        {
-            // Display error message using the custom function
-            display_chdir_error(command->args[1]);
-
-            // Set the exit status to indicate an error
-            check.exit_status = 1;
-            return 1; // Return an error code
-        }
-    }
-
-    // Get the current working directory and store it in current_working_dir
-    getcwd(current_working_dir, PATH_MAX);
-
-    // Update PWD and OLDPWD environment variables in the linked list
+// Function to update an environment variable's value
+void update_env_variable(t_environment *env_vars, const char *name, const char *value) {
     t_environment *current_env_elem = env_vars;
-    while (current_env_elem && strcmp(current_env_elem->name, "PWD") != 0)
+    while (current_env_elem && strcmp(current_env_elem->name, name) != 0)
         current_env_elem = current_env_elem->next;
-    if (current_env_elem)
-    {
+    if (current_env_elem) {
         free(current_env_elem->data);
-        current_env_elem->data = ft_strdup(current_working_dir);
+        current_env_elem->data = ft_strdup(value);
     }
+}
 
-    // Update OLDPWD with the previous working directory
-    current_env_elem = env_vars;
-    while (current_env_elem && strcmp(current_env_elem->name, "OLDPWD") != 0)
-        current_env_elem = current_env_elem->next;
-    if (current_env_elem)
-    {
-        free(current_env_elem->data);
-        current_env_elem->data = ft_strdup(old_working_dir);
-    }
-
-    // Convert the linked list of environment variables to an array
-    current_env_elem = env_vars;
-    char **env_array = ft_calloc(current_env_elem->count + 1, sizeof(char *));
+// Function to convert environment list to an array
+char **convert_env_list_to_array(t_environment *env_vars) {
+    char **env_array = ft_calloc(env_vars->count + 1, sizeof(char *));
     int i = 0;
 
-    if (env_array)
-    {
-        while (current_env_elem)
-        {
+    if (env_array) {
+        t_environment *current_env_elem = env_vars;
+        while (current_env_elem) {
             // Create entries for environment array in the form "NAME=VALUE"
             env_array[i] = ft_strjoin(current_env_elem->name, "=");
             if (current_env_elem->data)
@@ -110,18 +52,47 @@ int our_cd(t_cmd *command, char ***environment)
             current_env_elem = current_env_elem->next;
             i++;
         }
-        env_array[i] = NULL; // Set the last element to NULL to terminate the array
+        env_array[i] = NULL;
     }
-
-    // Update the environment with the new environment array
-    *environment = env_array;
-
-    // Clean up dynamic memory allocated for path strings
-    free(current_working_dir);
-    free(old_working_dir);
-
-    // Update exit status and return success code
-    check.exit_status = 0; // Set exit status to indicate success
-    return 0; // Return success code
+    return env_array;
 }
 
+// Handle changing directory to the specified path
+int handle_change_directory(const char *path, char ***environment) {
+    if (change_directory(path, environment) != 0)
+        return 1;
+
+    char current_working_dir[PATH_MAX];
+    getcwd(current_working_dir, PATH_MAX);
+
+    return current_working_dir;
+}
+
+// Handle updating PWD and OLDPWD environment variables
+void handle_update_env_variables(t_environment *env_vars, const char *current_working_dir, const char *old_working_dir) {
+    update_env_variable(env_vars, "PWD", current_working_dir);
+    update_env_variable(env_vars, "OLDPWD", old_working_dir);
+}
+
+// Custom implementation of the cd command
+int our_cd(t_cmd *command, char ***environment) {
+    t_environment *env_vars = create_env_vars(*environment);
+
+    char current_working_dir[PATH_MAX];
+    char old_working_dir[PATH_MAX];
+
+    if (!command->args[1]) {
+        const char *home_dir = expand_vars("$HOME", *environment);
+        return handle_change_directory(home_dir, environment);
+    } else {
+        return handle_change_directory(command->args[1], environment);
+    }
+
+    handle_update_env_variables(env_vars, current_working_dir, old_working_dir);
+
+    char **env_array = convert_env_list_to_array(env_vars);
+    *environment = env_array;
+
+    check.exit_status = 0;
+    return 0;
+}
