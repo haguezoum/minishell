57860c556ca0f4d2014ed89baf8da16697cb68b4
Hassan Aguezoum (Voodoo Child) <haguezoum@gmail.com>
2023-08-28 18:31:36 +0100

    fix export

--- src/our_export.c
+++ src/our_export.c
@@ -22,7 +22,12 @@ int is_valid_env_var_name(char *str) {
 int update_env_var(t_environment *env, char *name, char *data) {
     t_environment *tmp = env->next;
 
+    // printf("name: %s\n", name);
+    // printf("data: %s\n", data);
+    // printf("env->count: %d\n", env->count);
+    // printf("env->environment_array: %s\n", env->environment_array[0]);
     // Search for the existing environment variable
+    // exit(0);
     while (tmp) {
         if (ft_strcmp(tmp->name, name) == 0) {
             // Update the existing variable's value
@@ -56,69 +61,126 @@ int update_env_var(t_environment *env, char *name, char *data) {
     return 1; // Variable added
 }
 
+char *get_key(char *arg)
+{
+    char *name = NULL;
+    char *equal_sign = ft_strchr(arg, '=');
+
+    if (equal_sign) 
+    {
+        name = ft_strndup(arg, equal_sign - arg);
+    }
+    else 
+    {
+        name = ft_strdup(arg);
+    }
+
+    return name;
+}
+
+
+char *get_value(char *arg)
+{
+    char *data = NULL;
+    char *equal_sign = ft_strchr(arg, '=');
+    if (equal_sign) 
+    {
+        data = ft_strdup(equal_sign + 1); // allocate memory for data and copy it from equal_sign + 1
+    }
+    return data;
+}
+// char *get_value(char *arg)
+// {
+//     char *equal_sign = strchr(arg, '=');
+//     if (equal_sign)
+//     {
+//         char *value_start = equal_sign + 1;
+
+//         // Check if the value is enclosed in double quotes
+//         if (*value_start == '\"')
+//         {
+//             char *value_end = strchr(value_start + 1, '\"');
+//             if (value_end)
+//             {
+//                 return strndup(value_start + 1, value_end - value_start - 1);
+//             }
+//         }
+//         else
+//         {
+//             return strdup(value_start);
+//         }
+//     }
+//     return NULL;
+// }
 // Implementation of our_export
-int our_export(t_cmd *command, t_environment *env) {
+int our_export(char *command, t_environment *env) 
+{
     int exit_status = EXIT_SUCCESS;
 
     // If no arguments, print all environment variables
-    if (!command->args[1]) {
+    if (!command) 
+    {
         t_environment *tmp = env->next;
-        while (tmp) {
-            if (ft_strchr(tmp->name, '='))
-                printf("declare -x %s\n", tmp->name); // Print variable with declaration
+        while (tmp) 
+        {
+            if (tmp->data)
+            {
+                printf("%s=%s\n", tmp->name, tmp->data );
+                // printf("%s\n", tmp->data);
+            }
+            else
+                printf("%s", tmp->name);
             tmp = tmp->next;
         }
         return exit_status;
     }
 
-    // Loop through arguments and process each one
-    int i = 1;
-    while (command->args[i]) {
-        char *arg = command->args[i];
-        char *name = arg;
+        char *arg = command; //->args[i]; 
+        char *name = NULL;
         char *data = NULL;
-
-        // Separate name and data parts if '=' exists
-        char *equal_sign = ft_strchr(arg, '=');
-        if (equal_sign) {
-            name = ft_strndup(arg, equal_sign - arg);
-            data = ft_strdup(equal_sign + 1);
-        }
-
-   // Check if the name is a valid environment variable name
-if (!is_valid_env_var_name(name)) {
-    fprintf(stderr, "minishell: export: `%s': not a valid identifier\n", arg);
-    exit_status = EXIT_FAILURE;
-    if (data)
-        free(data);
-} else {
-    // Update or create the environment variable
-    if (data) {
-        printf("......\n");
-        if (!update_env_var(env, name, data)) {
-            perror("minishell: export");
+        
+        name = get_key(arg);
+        data = get_value(arg);
+        // Check if the name is a valid environment variable name
+        if (name && !is_valid_env_var_name(name)) 
+        {
+            fprintf(stderr, "minishell: export: `%s': not a valid identifier\n", arg);
             exit_status = EXIT_FAILURE;
+            if (data)
+                free(data);
         }
-        else {
-            for (int i = 0; env->environment_array[i]; i++)
-                printf("env: %s\n", env->environment_array[i]);
+        else 
+        {
+            // Update or create the environment variable
+            if (data) 
+            {
+                printf("......\n");
+                if (!update_env_var(env, name, data)) 
+                {
+                    perror("minishell: export");
+                    exit_status = EXIT_FAILURE;
+                }
+                else 
+                {
+                    if (name) {
+                        update_env_var(env, name, data);
+                    }
+                    // for (int i = 0; env->environment_array[i]; i++)
+                    //     printf("env: %s\n", env->environment_array[i]);
+                }
+                free(data);
+            } 
+            else 
+            {
+                if (!update_env_var(env, name, "")) 
+                {
+                    perror("minishell: export");
+                    exit_status = EXIT_FAILURE;
+                }
+            }
         }
-        free(data);
-    } else {
-        if (!update_env_var(env, name, "")) {
-            perror("minishell: export");
-            exit_status = EXIT_FAILURE;
-        }
-    }
-}
-if (equal_sign)
-    // free(name);
-
-        // if (data)
-        //     free(data);
-
-        i++;
-    }
+    //    i++;
+    // }
 
     return exit_status;
 }
