477936f7d183ddbfa75181dff55cb307cdaf1fbe
Hassan Aguezoum (Voodoo Child) <haguezoum@gmail.com>
2023-08-30 15:54:22 +0100

    fix norminette

--- src/exection/builtin_utils.c
+++ src/exection/builtin_utils.c
@@ -6,47 +6,56 @@
 /*   By: haguezou <haguezou@student.1337.ma >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 17:56:26 by haguezou          #+#    #+#             */
-/*   Updated: 2023/08/29 20:39:01 by haguezou         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:52:09 by haguezou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void export(t_cmd *ptr, t_environment *env)
+void	export_utils(t_cmd *ptr, t_environment *env, char *arg)
 {
-    char *es;
-    if (!ptr->args[1]) {
-        t_environment *tmp = env->next;
-        while (tmp) 
-        {
-            if (tmp->data) 
-            {
-                printf("declare -x %s=\"%s\"\n", tmp->name, tmp->data);
-            }
-            else
-                printf("%s\n", tmp->name);
-            tmp = tmp->next;
-        }
-        return ;
-    }
-    if(!ptr->args[2])
-    {
-        es = ft_strchr(ptr->args[1], '=');
-        if (!es)
-        {
-            printf("es : %s\n", es);
-            our_export(ptr->args[1], env);
-            return ;
-        }
-        char *arg2 = ft_strdup(es + 1);
-        char *arg1 = ft_substr(ptr->args[1], 0, ft_strlen(ptr->args[1]) - ft_strlen(es));
-        char *str = ft_strjoin(arg1, "=");
-        str = ft_strjoin(str, arg2);
-        our_export(str, env);
-    }
-    else
-    {
-        char *str = ft_strjoin(ptr->args[1], ptr->args[2]);
-        our_export(str, env);
-    }
+	char	*es;
+	char	*str;
+	char	*arg2;
+	char	*arg1;
+
+	es = ft_strchr(arg, '=');
+	if (!es)
+	{
+		our_export(arg, env);
+		return ;
+	}
+	arg2 = ft_strdup(es + 1);
+	arg1 = ft_substr(arg, 0, ft_strlen(arg) - ft_strlen(es));
+	str = ft_strjoin(arg1, "=");
+	str = ft_strjoin(str, arg2);
+	our_export(str, env);
+}
+
+void	export(t_cmd *ptr, t_environment *env)
+{
+	char			*es;
+	t_environment	*tmp;
+	char			*str;
+
+	if (!ptr->args[1])
+	{
+		tmp = env->next;
+		while (tmp)
+		{
+			if (tmp->data)
+				printf("declare -x %s=\"%s\"\n", tmp->name, tmp->data);
+			else
+				printf("%s\n", tmp->name);
+			tmp = tmp->next;
+		}
+		return ;
+	}
+	if (!ptr->args[2])
+		export_utils(ptr, env, ptr->args[1]);
+	else
+	{
+		str = ft_strjoin(ptr->args[1], ptr->args[2]);
+		our_export(str, env);
+	}
 }
