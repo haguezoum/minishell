28eec3840d4e83f781f22e0ba093de3af6e4b46c
Hassan Aguezoum (Voodoo Child) <haguezoum@gmail.com>
2023-08-30 15:59:35 +0100

    fix norminette

--- src/exection/signales.c
+++ src/exection/signales.c
@@ -6,20 +6,22 @@
 /*   By: haguezou <haguezou@student.1337.ma >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 21:57:04 by haguezou          #+#    #+#             */
-/*   Updated: 2023/08/30 12:41:30 by haguezou         ###   ########.fr       */
+/*   Updated: 2023/08/30 15:55:26 by haguezou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void exit_signal();
-void handel_signal(int sig)
+void	exit_signal(void);
+
+void	handel_signal(int sig)
 {
-    if(sig == SIGINT)
-        exit_signal();
+	if (sig == SIGINT)
+		exit_signal();
 }
-void exit_signal()
+
+void	exit_signal(void)
 {
-    ft_putstr_fd("\n", 1);
-    ft_putstr_fd("\033[1;32mminishell$ \033[0m", 1);
+	ft_putstr_fd("\n", 1);
+	ft_putstr_fd("\033[1;32mminishell$ \033[0m", 1);
 }
