7eb235b648d5cc94c9626f4de3043fc127bbbc2b
Hassan Aguezoum (Voodoo Child) <haguezoum@gmail.com>
2023-09-02 15:41:05 +0100

    fix expansion of multipl env variables with quote and also in DEFAULT status

--- src/parser/parse_cmd.c
+++ src/parser/parse_cmd.c
@@ -3,81 +3,143 @@
 /*                                                        :::      ::::::::   */
 /*   parse_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aet-tass <aet-tass@student.42.fr>          +#+  +:+       +#+        */
+/*   By: haguezou <haguezou@student.1337.ma >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 18:56:24 by aet-tass          #+#    #+#             */
-/*   Updated: 2023/08/29 19:34:43 by aet-tass         ###   ########.fr       */
+/*   Updated: 2023/09/02 15:35:12 by haguezou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void handle_regular_argument(t_global **token, char **arguments, int *i, int *ignore_arguments) {
-    if (!(*ignore_arguments)) {
+void    handle_regular_argument(t_global **token, char **arguments, int *i,
+        int *ignore_arguments)
+{
+    if (!(*ignore_arguments))
+    {
         arguments[*i] = ft_strndup((*token)->content, (*token)->size);
         (*i)++;
     }
     *token = (*token)->next_token;
 }
 
-void handle_quoted_argument(t_global **token, t_environment *env, char **arguments, int *i, int *ignore_arguments) {
-    if (!(*ignore_arguments)) {
+void    handle_quoted_argument(t_global **token, t_environment *env,
+        char **arguments, int *i, int *ignore_arguments)
+{
+    if (!(*ignore_arguments))
+    {
         arguments[*i] = parse_quoted_argument(token, env);
-        if (arguments[*i]) {
+        if (arguments[*i])
+        {
             (*i)++;
         }
-    } else {
+    }
+    else
+    {
         *token = (*token)->next_token;
     }
 }
 
-void handle_env_argument(t_global **token, t_environment *env, char **arguments, int *i, int *ignore_arguments) {
-    if (!(*ignore_arguments)) {
-        arguments[*i] = store_vars((*token)->content + 1, env);
-        if (arguments[*i]) {
-            (*i)++;
-        }
+void    ft_free_split(char **split)
+{
+    int i;
+
+    i = 0;
+    while (split[i])
+    {
+        free(split[i]);
+        i++;
     }
-    *token = (*token)->next_token;
+    free(split);
 }
 
-void handle_other_token(t_global **token, t_environment *env, t_rlist *redir, int *ignore_arguments) {
-    enum e_token type = (*token)->type;
-    if (check_redir(type)) {
+void    handle_env_argument(t_global **token, t_environment *env,
+        char **arguments, int *i, int *ignore_arguments)
+{
+    if (!(*ignore_arguments))
+    {
+        char **split = ft_split((*token)->content, '$');
+        int j = 0;
+        while (split[j])
+        {
+            char *first_line = ft_strtrim(split[j], " ");
+            char *line = ft_strtrim(first_line, "\"");
+            free(first_line);
+            arguments[*i] = store_vars(line, env);
+            free(line);
+            if (arguments[*i])
+            {
+                (*i)++;
+            }
+            j++;
+        }
+
+        j = 0;
+        while (split[j])
+        {
+            *token = (*token)->next_token;
+            j++;
+        }
+        if (*token)
+            *token = (*token)->next_token;
+        ft_free_split(split);
+    }
+}
+
+
+void    handle_other_token(t_global **token, t_environment *env, t_rlist *redir,
+        int *ignore_arguments)
+{
+    enum e_token    type;
+
+    type = (*token)->type;
+    if (check_redir(type))
+    {
         *ignore_arguments = 1;
-        if (!parse_redir(token, env, redir)) {
-            return;
+        if (!parse_redir(token, env, redir))
+        {
+            return ;
         }
-    } else {
+    }
+    else
+    {
         *token = (*token)->next_token;
     }
 }
+int    parse_command_arguments(t_global **token, t_environment *env,
+        t_rlist *redir, char **arguments)
+{
+    int                i;
+    int                ignore_arguments;
+    enum e_token    first_type;
 
-int parse_command_arguments(t_global **token, t_environment *env, t_rlist *redir, char **arguments) {
-    int i = 0;
-    int ignore_arguments = 0;
-
-    enum e_token first_type = (*token)->type;
-    if (check_redir(first_type)) {
-        if (!parse_redir(token, env, redir)) {
-            return EXIT_FAILURE;
+    i = 0;
+    ignore_arguments = 0;
+    first_type = (*token)->type;
+    if (check_redir(first_type))
+    {
+        if (!parse_redir(token, env, redir))
+        {
+            return (EXIT_FAILURE);
         }
     }
-
-    while (*token && (*token)->type != PIPE_LINE) {
-        if (is_whitespace_tokene(*token)) {
+    while (*token && (*token)->type != PIPE_LINE)
+    {
+        if (is_whitespace_tokene(*token))
             skip_whitespace_tokene(token);
-        } else if ((*token)->type == WORD) {
+        else if ((*token)->type == WORD)
             handle_regular_argument(token, arguments, &i, &ignore_arguments);
-        } else if ((*token)->type == DQUOTE || (*token)->type == SQUOTE) {
-            handle_quoted_argument(token, env, arguments, &i, &ignore_arguments);
-        } else if ((*token)->type == ENV) {
+        else if ((*token)->type == ENV)
+        {
             handle_env_argument(token, env, arguments, &i, &ignore_arguments);
-        } else {
-            handle_other_token(token, env, redir, &ignore_arguments);
+            break ;
         }
+        else if ((*token)->type == SQUOTE || (*token)->type == DQUOTE)
+            handle_quoted_argument(token, env, arguments, &i,
+                    &ignore_arguments);
+        else
+            handle_other_token(token, env, redir, &ignore_arguments);
     }
     arguments[i] = NULL;
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
 }
-
